ec76ce2c16a1818e1018a910f873f4dfe4b67480
TIEUBEA <borismoussa5@gmail.com>
2023-12-02 01:00:23 +0100

    OK

---
+++ src/main/java/tech/enspd/enspdbiblio/EnspdBiblioApplication.java
@@ -0,0 +1,13 @@
+package tech.enspd.enspdbiblio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnspdBiblioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnspdBiblioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/enspd/enspdbiblio/EnspdBiblioApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.enspd.enspdbiblio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnspdBiblioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
